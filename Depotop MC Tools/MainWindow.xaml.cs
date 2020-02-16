33092eb9219891d67153badda3b2be4d28c94ffb
Hohlkehle <enebaybe@gmail.com>
2020-02-16 13:15:22 +0200

    First commit

---
+++ Depotop MC Tools/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Depotop_MC_Tools
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> m_WordToKey = new List<string>() { "FR", "EN", "RU", "DE", "IT" };
+        private List<string> m_AutoList = new List<string>() { "TOYOTA", "FORD", "CHEVROLET", "NISSAN", "KIA", "MERCEDES", "BMW", "OPEL", "MAZDA", "VOLKSWAGEN", "CITROEN", "VOLVO", "SKODA", "LAND", "RENAULT", "HONDA", "MITSUBISHI", "AUDI", "JEEP", "PEUGEOT", "LEXUS", "SUZUKI", "INFINITI", "FIAT", "MINI", "ALFA", "VW", "HYUNDAI", "CHRYSLER", "SAAB", "HUMMER", "JAGUAR", "CADILLAC", "SUBARU", "DAIHATSU", "DACIA", "DODGE", "SSANGYONG", "LANCIA", "ISUZU", "SSANG", "SMART", "PORSCHE", "IVECO", "SEAT", "DAEWOO", "MCLAREN" };
+
+        private Dictionary<string, string[]> m_SidesLang = new Dictionary<string, string[]>()
+        {
+            { "avant", new string[] {"avant", "front", "передний", "vorne", "anteriore"} },
+            { "arriére", new string[] {"arriére", "rear", "задний", "hinten", "posteriore"} },
+            { "arriere", new string[] {"arriére", "rear", "задний", "hinten", "posteriore"} },
+            { "supérieur", new string[] {"supérieur", "upper", "верхний", "oben", "superiore"} },
+            { "superieur", new string[] {"supérieur", "upper", "верхний", "oben", "superiore"} },
+            { "inferieur", new string[] {"inferieur", "lower", "нижний", "unten", "inferiore"} },
+            { "droite", new string[] {"droite", "right", "правый", "rechts", "destro"} },
+            { "gauche", new string[] {"gauche", "left", "левый", "links", "sinistro"} },
+            { "longitudinal", new string[] {"longitudinal", "longitudinal", "продольный", "entlang", "longitudinale"} },
+            { "transversal", new string[] {"transversal", "transverse", "поперечный", "quer", "trasversale"} },
+            { "pour", new string[] {"pour", "for", "для", "für", "per"} }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            for (var i = 0; i < TbInputTitles.LineCount; i++)
+            {
+                var line = TbInputTitles.GetLineText(i);
+                TbInputTitles.Text += line + "\r\n";
+            }
+        }
+    }
+}
